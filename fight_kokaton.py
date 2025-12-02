f87d93f5872ffa19f1018a117b18b6496fb3c7da
Haruya Iwama <c0c2400768@edu.teu.ac.jp>
2025-12-02 16:28:04 +0900

    向きに応じたビーム実装

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -3,6 +3,7 @@ import random
 import sys
 import time
 import pygame as pg
+import math
 
 
 WIDTH = 1100  # ゲームウィンドウの幅
@@ -56,6 +57,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5, 0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -82,6 +84,7 @@ class Bird:
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.img = __class__.imgs[tuple(sum_mv)]
+            self.dire = tuple(sum_mv)
         screen.blit(self.img, self.rct)
 
 
@@ -94,11 +97,17 @@ class Beam:
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん（Birdインスタンス）
         """
-        self.img = pg.image.load(f"fig/beam.png")
-        self.rct = self.img.get_rect()
-        self.rct.centery = bird.rct.centery  # こうかとんの中心＝ビームの中心
-        self.rct.left = bird.rct.right # こうかとんの右端＝ビームの左端
-        self.vx, self.vy = +5, 0 # ビームの速度ベクトル
+        base = pg.image.load(f"fig/beam.png").convert_alpha()
+        vx, vy = bird.dire
+        self.vx, self.vy = vx, vy # ビームの速度ベクトル
+        theta = math.atan2(-vy, vx)  # 速度ベクトルの角度（ラジアン）
+        deg = math.degrees(theta)  # 角度（度）
+        self.img = pg.transform.rotozoom(base, deg, 1.0)
+        cx = bird.rct.centerx + bird.rct.width  * (self.vx / 5)
+        cy = bird.rct.centery + bird.rct.height * (self.vy / 5)
+        self.rct = self.img.get_rect(center=(cx, cy))
+
+
 
     def update(self, screen: pg.Surface):
         """
